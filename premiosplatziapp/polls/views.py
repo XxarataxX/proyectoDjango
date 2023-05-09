337cf9f7682a340731b748a8bae796e9cd67ffa9
arata <al0291575@tecmilenio.mx>
2023-05-09 11:28:39 -0600

    funcionalidad formularios completa

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,20 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+
+from polls.models import Question, Choice
 
 
 def index(request):
-    return HttpResponse("estas en la pagina principal de premios platzi app")   
+    lates_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": lates_question_list
+    })
 
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
-
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
 
 def results(request, question_id):
-    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
-
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, "polls/results.html", {
+        "question": question
+    })
 
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando a la pregunta {question_id}")
-
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "no elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        
 
