b2313223a320da45a011b22d3dfac59c3be66474
arata <al0291575@tecmilenio.mx>
2023-05-09 09:22:07 -0600

    tercer commit

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("hola mundo")   
+    return HttpResponse("estas en la pagina principal de premios platzi app")   
 
-# Create your views here.
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta {question_id}")
+
+
